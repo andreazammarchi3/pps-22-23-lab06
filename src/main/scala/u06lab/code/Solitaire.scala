90771a4a11cdc012f5478c1c26dcbdb22e3f4bd8
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-04-19 18:30:21 +0200

    Complete ex3

--- src/main/scala/u06lab/code/Solitaire.scala
+++ src/main/scala/u06lab/code/Solitaire.scala
@@ -1,15 +1,60 @@
 package u06lab.code
 
+import scala.::
+
+object Domain:
+  case class Position(x: Int, y: Int)
+
+  case class BoardSize(width: Int, height: Int):
+    def center: Position = Position(width / 2, height / 2)
+    def total: Int = width * height
+    def widthRange: Range = 0 until width
+    def heightRange: Range = 0 until height
+
+  case class Board(size: BoardSize, positions: Seq[Position])
+
+  case class Solution(positions: Seq[Position]):
+    export positions.{last, contains, indexOf}
+    def :+(position: Position): Solution = Solution(positions :+ position)
+
+  object Solution:
+    def center(size: BoardSize): Solution = Solution(size.center)
+
+    def apply(position: Position, positions: Position*): Solution = new Solution(position +: positions)
+    def apply(positions: Seq[Position]): Solution = new Solution(positions)
+
 object Solitaire extends App:
-  def render(solution: Seq[(Int, Int)], width: Int, height: Int): String =
-    val reversed = solution.reverse
+
+  import Domain.*
+
+  @main def main(): Unit =
+    val boardSize = BoardSize(5, 5)
+    val solutions = placeMarks(boardSize, boardSize.total / 2 + 1)
+    solutions.foreach(p => println(render(p, boardSize.width, boardSize.height) + "\n"))
+    println("Solutions: " + solutions.size)
+
+  private def placeMarks(size: BoardSize, n: Int): Seq[Solution] = n match
+    case 1 => Seq(Solution.center(size))
+    case _ =>
+      for
+        solution <- placeMarks(size, n - 1)
+        x <- size.widthRange
+        y <- size.heightRange
+        spot = Position(x, y)
+        if !solution.contains(spot)
+        if isLegalMove(solution, spot)
+      yield solution :+ spot
+
+  // Max distance horizontally and vertically by 2 or diagonally 1
+  private def isLegalMove(solution: Solution, position: Position) =
+    ((solution.last.x - position.x).abs + (solution.last.y - position.y).abs) == 2
+
+  private def render(solution: Solution, width: Int, height: Int): String =
+    val reversed = solution //.reverse
     val rows =
       for y <- 0 until height
           row = for x <- 0 until width
-          number = reversed.indexOf((x, y)) + 1
+                    number = reversed.indexOf(Position(x, y)) + 1
           yield if number > 0 then "%-2d ".format(number) else "X  "
       yield row.mkString
     rows.mkString("\n")
-
-
-  println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
