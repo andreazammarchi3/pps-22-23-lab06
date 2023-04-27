1b3a67570f0fdc024af6f0004aaacbd3944bcc94
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-04-27 18:41:51 +0200

    Update Solitaire.scala

--- src/main/scala/u06lab/code/Solitaire.scala
+++ src/main/scala/u06lab/code/Solitaire.scala
@@ -1,60 +1,42 @@
 package u06lab.code
 
-import scala.::
-
-object Domain:
-  case class Position(x: Int, y: Int)
-
-  case class BoardSize(width: Int, height: Int):
-    def center: Position = Position(width / 2, height / 2)
-    def total: Int = width * height
-    def widthRange: Range = 0 until width
-    def heightRange: Range = 0 until height
-
-  case class Board(size: BoardSize, positions: Seq[Position])
-
-  case class Solution(positions: Seq[Position]):
-    export positions.{last, contains, indexOf}
-    def :+(position: Position): Solution = Solution(positions :+ position)
-
-  object Solution:
-    def center(size: BoardSize): Solution = Solution(size.center)
-
-    def apply(position: Position, positions: Position*): Solution = new Solution(position +: positions)
-    def apply(positions: Seq[Position]): Solution = new Solution(positions)
+import javax.swing.text.Position
 
 object Solitaire extends App:
+  private type Position = (Int, Int)
+  private type Solution = List[Position]
+  private def placeMarks(width: Int, height: Int)(init: Position): Iterable[Solution] =
+    def _placeMarks(n: Int): Iterable[Solution] = n match
+      case 1 => Seq(List(init)).view
+      case _ =>
+        for
+          solution <- _placeMarks(n - 1)
+          i <- 0 until width
+          j <- 0 until height
+          newPos = (i, j)
+          if !solution.contains(newPos)
+          if isSafe(newPos, solution.head)
+        yield newPos :: solution
 
-  import Domain.*
+    _placeMarks(width * height)
 
-  @main def main(): Unit =
-    val boardSize = BoardSize(5, 5)
-    val solutions = placeMarks(boardSize, boardSize.total / 2 + 1)
-    solutions.foreach(p => println(render(p, boardSize.width, boardSize.height) + "\n"))
-    println("Solutions: " + solutions.size)
+  private def isSafe(p1: Position, p2: Position): Boolean =
+    (math.abs(p1._1 - p2._1) == 2 && math.abs(p1._2 - p2._2) == 2)
+      || (math.abs(p1._1 - p2._1) == 3 && math.abs(p1._2 - p2._2) == 0)
+      || (math.abs(p1._1 - p2._1) == 0 && math.abs(p1._2 - p2._2) == 3)
 
-  private def placeMarks(size: BoardSize, n: Int): Seq[Solution] = n match
-    case 1 => Seq(Solution.center(size))
-    case _ =>
-      for
-        solution <- placeMarks(size, n - 1)
-        x <- size.widthRange
-        y <- size.heightRange
-        spot = Position(x, y)
-        if !solution.contains(spot)
-        if isLegalMove(solution, spot)
-      yield solution :+ spot
-
-  // Max distance horizontally and vertically by 2 or diagonally 1
-  private def isLegalMove(solution: Solution, position: Position) =
-    ((solution.last.x - position.x).abs + (solution.last.y - position.y).abs) == 2
-
-  private def render(solution: Solution, width: Int, height: Int): String =
-    val reversed = solution //.reverse
+  private def render(solution: Seq[(Int, Int)], width: Int, height: Int): String =
+    val reversed = solution.reverse
     val rows =
       for y <- 0 until height
           row = for x <- 0 until width
-                    number = reversed.indexOf(Position(x, y)) + 1
+                    number = reversed.indexOf((x, y)) + 1
           yield if number > 0 then "%-2d ".format(number) else "X  "
       yield row.mkString
     rows.mkString("\n")
+
+  private val size = (5, 7)
+  private val center: Position = (size._1 / 2, size._2 / 2)
+  private val solutions = placeMarks(size._1, size._2)(center)
+  for s <- solutions
+    do println(render(solution = s, width = size._1, height = size._2) + "\n")
